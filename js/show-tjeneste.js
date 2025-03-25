7265b19c405c16bc0de02dddfa759e3cd75908c0
magn3054 <mdch20@student.aau.dk>
2025-03-26 00:23:46 +0100

    links added

--- js/show-tjeneste.js
+++ js/show-tjeneste.js
@@ -4,6 +4,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const logoContainer = document.getElementById("logos");
     const selectAllButton = document.getElementById("selectAllButton");
 
+    const platformLinks = {
+        netflix: "https://www.netflix.com/dk/",
+        disney: "https://www.disneyplus.com/da-dk",
+        hbo: "https://play.max.com/",
+        prime: "https://www.primevideo.com/offers/nonprimehomepage/ref=dv_web_force_root",
+        viaplay: "https://viaplay.dk/dk-da/",
+        tv2: "https://play.tv2.dk/",
+        dr1: "https://www.dr.dk/drtv/"
+    };
+
     // Load saved selections from localStorage
     const savedPlatforms = JSON.parse(localStorage.getItem("selectedPlatforms")) || [];
 
@@ -28,10 +38,13 @@ document.addEventListener("DOMContentLoaded", function () {
         const logoCount = selectedPlatforms.length;
         const logoWidth = logoCount > 0 ? Math.min(15, 15 / Math.sqrt(logoCount + 2) + 5) : 15;
 
+        const website = ["dr1", "tv2", "disney", "viaplay", "hbo", "netflix", "prime"];
+
         // Display logos
         selectedPlatforms.forEach(platform => {
-            const imgContainer = document.createElement("div");
+            const imgContainer = document.createElement("a");
             imgContainer.className = "logo-container";
+            imgContainer.href = platformLinks[platform] || "#";
             logoContainer.appendChild(imgContainer);
             const img = document.createElement("img");
             img.src = `img/${platform}.svg`; // Path to logo images
